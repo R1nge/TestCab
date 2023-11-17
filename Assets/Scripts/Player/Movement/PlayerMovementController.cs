ee1bee495edda6caa6dc638abd505b86ced09c14
R1nge <r1ngegamedev@gmail.com>
2023-11-17 16:02:59 +0300

    Player movement

---
+++ Assets/Scripts/Player/Movement/PlayerMovementController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Player.Movement
+{
+    public class PlayerMovementController
+    {
+        private readonly NavMeshAgent _player;
+
+        public PlayerMovementController(NavMeshAgent player)
+        {
+            _player = player;
+        }
+
+        public void MoveTo(Vector3 position)
+        {
+            _player.SetDestination(position);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Movement/PlayerMovementView.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+namespace Player.Movement
+{
+    public class PlayerMovementView : MonoBehaviour
+    {
+        [SerializeField] private LayerMask ignore;
+        [SerializeField] private NavMeshAgent navMeshAgent;
+        [SerializeField] private Camera playerCamera;
+        private PlayerMovementController _playerMovementController;
+
+        private void Awake()
+        {
+            _playerMovementController = new PlayerMovementController(navMeshAgent);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    Ray screenPointToRay = playerCamera.ScreenPointToRay(Input.mousePosition);
+                
+                    if (Physics.Raycast(screenPointToRay, out RaycastHit hit, Mathf.Infinity, layerMask: ~ignore))
+                    {
+                        _playerMovementController.MoveTo(hit.point);
+                    }
+                }
+            }
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                {
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+
+                        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask: ~ignore))
+                        {
+                            _playerMovementController.MoveTo(hit.point);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
